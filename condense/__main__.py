55bb81b89bbe876e18a43e2a07dd8ab31fba08f2
Arker123 <kharbandarnav@gmail.com>
2024-02-17 17:02:58 +0530

    Enable Black and isort for Code Style Checking

---
+++ condense/__main__.py
@@ -0,0 +1,5 @@
+import sys
+
+from condense.main import main
+
+sys.exit(main())
---
+++ condense/version.py
@@ -0,0 +1,3 @@
+# Copyright (C) 2024 Condense, Inc. All Rights Reserved.
+# caution: this file gets overwritten when building using PyInstaller, don't add required data in here without handling
+__version__ = "0.0.1"
