6426a4f5924c6fa82721491ef0caec7e756c139a
hardik <hardik32904@gmail.com>
2024-02-19 21:31:33 +0530

    Fixes

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4751;
 
 app.use("/auth", authRoute);
 app.use("/note", noteRoute);
@@ -20,9 +20,11 @@ mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("Connected to Database"))
   .then(() => {
-    app.listen(port, () => {
-      console.log(`Server is running on ${port} `);
-    });
+    if (process.env.NODE_ENV != "test") {
+      app.listen(port, () => {
+        console.log(`Server is running on ${port} `);
+      });
+    }
   });
 
 module.exports = app;
