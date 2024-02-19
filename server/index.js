9695ab82a7353eda24d7dfc3682332ecfa9a9799
Arker123 <kharbandarnav@gmail.com>
2024-02-19 19:52:07 +0530

    Added ES LINT for nodejs

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const authRoute = require("./routes/authRoute");
-const mongoose = require("mongoose");
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const authRoute = require('./routes/authRoute');
+const mongoose = require('mongoose');
 
 const app = express();
 dotenv.config();
@@ -10,21 +10,21 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env)
+console.log(process.env);
 
 const port = process.env.PORT || 5000;
 
-app.use("/auth", authRoute);
+app.use('/auth', authRoute);
 
 mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => console.log("Connected to Database"))
-  .then(() => {
-    if(process.env.NODE_ENV != 'test'){
-      app.listen(port, () => {
-        console.log(`Server is running on ${port} `);
-      });
-    }
-  });
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log('Connected to Database'))
+    .then(() => {
+      if (process.env.NODE_ENV != 'test') {
+        app.listen(port, () => {
+          console.log(`Server is running on ${port} `);
+        });
+      }
+    });
 
 module.exports = app;
