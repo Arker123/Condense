1ff2a552c748cda74e5e63addb6d633c1c67f291
Virat <2021csb1220@iitrpr.ac.in>
2024-02-24 14:26:05 +0530

    ContactUs Page Added

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 import Landing from './pages/LandingPage';
 import LandingPage2 from './pages/LandingPage2';
+import ContactUs from './pages/ContactUs';
 
 /**
  * Renders the main application component.
@@ -13,6 +14,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Landing />} />
                 <Route exact path="/landing" element={<LandingPage2 />} />
+                <Route exact path="/contact" element={<ContactUs />} />
             </Routes>
         </div>
     );
---
+++ client/src/pages/ContactUs.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './ContactUs.css';
+
+
+function ContactUs() {
+  return (
+    <div className="App">
+      <div className='head'>
+        <div className="contactus-logo">
+          <img src="/images/logo_condense.jpg" alt="Logo" />
+          <h1> CONDENSE </h1> 
+        </div>
+        <div className='contact-container'>
+          <div className='contact-form'>
+            <h2>Contact Us</h2>
+            <form>
+            <input type="text" id="fullName" name="fullName" placeholder="Your Full Name" />
+            <input type="email" id="email" name="email" placeholder="Your Email Address" />
+            <textarea id="message" name="message" placeholder="Your Message"></textarea>
+            <button type="submit">Submit</button>
+            </form>
+          </div>
+          <div className='contact-pic'>
+            <img src="images/undraw_contact_us_re_4qqt (1).svg" alt="Contact" />
+          </div>
+        </div>        
+      </div>
+      <div className="contact-details">
+        <div className="contact-icon">
+          <FontAwesomeIcon icon={['fas', 'map-marker-alt']} />
+          <p> &nbsp; &nbsp; &nbsp;IIT ROPAR, Punjab</p>
+        </div>
+        <div className="contact-icon">
+          <FontAwesomeIcon icon={['fas', 'phone']} />
+          <p>&nbsp; &nbsp; &nbsp;+911234567890</p>
+        </div>
+        <div className="contact-icon">
+          <FontAwesomeIcon icon={['far', 'envelope']} />
+          <p> &nbsp; &nbsp; &nbsp;Team07@iitrpr.ac.in</p>
+        </div>
+      </div>
+      </div>
+  );
+}
+
+export default ContactUs;
