16fd3ebfc5f2bb33b1a08354bd418982e936c040
n1ti <niti200005@gmail.com>
2024-02-27 17:07:02 +0530

    Frontend testing setup and code coverage added

---
+++ client/src/__tests__/app.test.js
@@ -0,0 +1,11 @@
+import "jest-canvas-mock"; //Need because of lottie-files
+import {BrowserRouter as Router} from 'react-router-dom';
+import {cleanup, screen, render} from '@testing-library/react';
+import App from "../App.js";
+
+afterEach(cleanup);
+
+test('app renders without crashing', ()=>{
+    render (<Router><App /></Router>);
+});
+
