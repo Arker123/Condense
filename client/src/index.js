aa988597c0be95279df5326e92d9c7e9b89d3b79
Virat <2021csb1220@iitrpr.ac.in>
2024-03-02 21:33:37 +0530

    fixes

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,10 @@ import './index.css';
 import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom';
 // import reportWebVitals from './reportWebVitals';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faMapMarkerAlt, faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {faMapMarkerAlt, faPhone, faEnvelope}
+    from '@fortawesome/free-solid-svg-icons';
+import {far} from '@fortawesome/free-regular-svg-icons';
 
 library.add(faMapMarkerAlt, faPhone, faEnvelope, far);
 
