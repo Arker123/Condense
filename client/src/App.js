f8541baf58dbdbc5061e0c1e9946f9ca5170b322
Dhruv Singh Negi <2021csb1167@iitrpr.ac.in>
2024-03-03 15:11:34 +0530

    Added Route for SummaryPage

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,6 @@ import Landing from './pages/LandingPage';
 import LandingPage2 from './pages/LandingPage2';
 import ContactUs from './pages/ContactUs';
 import SignUp from './pages/SignUp';
-import ContactUs from './pages/ContactUs';
-import SignUp from './pages/SignUp';
 import SummaryPage from './pages/SummaryPage';
 
 /**
