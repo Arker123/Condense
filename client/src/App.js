6ff20cef90b1e3274ad8ff3102f5500b1d84b309
hardik <hardik32904@gmail.com>
2024-03-12 00:39:08 +0530

    Fixes

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import ContactUs from "./pages/ContactUs";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
-import Dashboard from './pages/Dashboard';
+import Dashboard from "./pages/Dashboard";
 import SignUp from "./pages/SignUp";
 import SummaryPage from "./pages/SummaryPage";
 import { store, persistor } from "./redux/store";
@@ -16,16 +16,21 @@ import { store, persistor } from "./redux/store";
  * @return {JSX.Element} The rendered App component.
  */
 function App() {
-    return (
-        <div className="">
-            <Routes>
-                <Route path="/" element={<Landing />} />
-                <Route exact path="/landing" element={<LandingPage2 />} />
-                <Route exact path="/contact" element={<ContactUs />} />
-                <Route exact path="/signup" element={<SignUp />} />
-            </Routes>
-        </div>
-    );
+  return (
+    <div className="">
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route exact path="/landing" element={<LandingPage2 />} />
+            <Route exact path="/contact" element={<ContactUs />} />
+            <Route exact path="/signup" element={<SignUp />} />
+            <Route exact path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </PersistGate>
+      </Provider>
+    </div>
+  );
 }
 
 export default App;
