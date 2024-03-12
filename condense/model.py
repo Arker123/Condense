5298986fde1f0b666ff86c7a56aebad37f7a1b32
ARinger22 <gautamarpit1204@gmail.com>
2024-03-12 16:15:35 +0530

    conversed code

--- condense/model.py
+++ condense/model.py
@@ -1,118 +1,117 @@
-#Libraries
+# Libraries
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import matplotlib
-matplotlib.use('module://ipykernel.pylab.backend_inline')
+import matplotlib.pyplot as plt
+
+matplotlib.use("module://ipykernel.pylab.backend_inline")
 import os
-
-# Import functions for data preprocessing & data preparation
-from sklearn.preprocessing import LabelEncoder
-from sklearn.utils import resample
-from sklearn.feature_extraction.text import CountVectorizer
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-
-from nltk.tokenize import word_tokenize
-from nltk.stem import WordNetLemmatizer
-from nltk.stem import PorterStemmer, LancasterStemmer
-from nltk.stem.snowball import SnowballStemmer
-from nltk.corpus import stopwords
-from nltk.corpus import wordnet
-import string
-from string import punctuation
-import nltk
 import re
-from sklearn.feature_extraction.text import CountVectorizer
+import sys
+from string import punctuation
+
+import nltk
+import emoji
+from nltk.stem import PorterStemmer, LancasterStemmer, WordNetLemmatizer
+from nltk.corpus import wordnet, stopwords
+from nltk.tokenize import word_tokenize
+from sklearn.utils import resample
+from sklearn.metrics import accuracy_score, confusion_matrix
+from nltk.stem.snowball import SnowballStemmer
 from sklearn.naive_bayes import GaussianNB
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.feature_extraction.text import CountVectorizer
 
-def text_processing(text):
-    text = text.lower()
-    text = re.sub(r'\n',' ', text)
-
-    text = re.sub('[%s]' % re.escape(punctuation), "", text)
-    text = re.sub("^a-zA-Z0-9$,.", "", text)
-
-    text = re.sub(r'\s+', ' ', text, flags=re.I)
-    text = re.sub(r'\W', ' ', text)
-
-    text = ' '.join([word for word in word_tokenize(text) if word not in stop_words])
-    text=' '.join([lzr.lemmatize(word) for word in word_tokenize(text)])
-    return text
-
-data = pd.read_csv('comments.csv')
-data.columns
-data1=data.drop(['author_display_name','comment_like_count'],axis=1)
-data1
-
-nltk.download('vader_lexicon')
-sentiments = SentimentIntensityAnalyzer()
-data1["Positive"] = [sentiments.polarity_scores(i)["pos"] for i in data1["comment"]]
-data1["Negative"] = [sentiments.polarity_scores(i)["neg"] for i in data1["comment"]]
-data1["Neutral"] = [sentiments.polarity_scores(i)["neu"] for i in data1["comment"]]
-data1['Compound'] = [sentiments.polarity_scores(i)["compound"] for i in data1["comment"]]
-score = data1["Compound"].values
-sentiment = []
-for i in score:
-    if i >= 0.05 :
-        sentiment.append('Positive')
-    elif i <= -0.05 :
-        sentiment.append('Negative')
-    else:
-        sentiment.append('Neutral')
-data1["Sentiment"] = sentiment
-data1.head()
-
-data2=data1.drop(['Positive','Negative','Neutral','Compound'],axis=1)
-data2.head()
-
-nltk.download('stopwords')
-stop_words = stopwords.words('english')
+nltk.download("wordnet")
+nltk.download("stopwords")
+stop_words = stopwords.words("english")
 porter_stemmer = PorterStemmer()
 lancaster_stemmer = LancasterStemmer()
 snowball_stemer = SnowballStemmer(language="english")
 lzr = WordNetLemmatizer()
 
-nltk.download('omw-1.4')
-data_copy = data2.copy()
-data_copy.Comment = data_copy.Comment.apply(lambda text: text_processing(text))
 
-le = LabelEncoder()
-data_copy['Sentiment'] = le.fit_transform(data_copy['Sentiment'])
+def text_processing(text):
+    text = text.lower()
+    text = emoji.demojize(text)
+    text = re.sub(r"\n", " ", text)
 
-processed_data = {
-    'Sentence':data_copy.Comment,
-    'Sentiment':data_copy['Sentiment']
-}
+    text = re.sub("[%s]" % re.escape(punctuation), "", text)
+    text = re.sub("^a-zA-Z0-9$,.", "", text)
+    text = re.sub(r"[^\w\s\:\d]", "", text)
+    text = re.sub(r"\s+", " ", text, flags=re.I)
+    text = re.sub(r"\W", " ", text)
 
-processed_data = pd.DataFrame(processed_data)
-processed_data.head()
-
-df_neutral = processed_data[(processed_data['Sentiment']==1)]
-df_negative = processed_data[(processed_data['Sentiment']==0)]
-df_positive = processed_data[(processed_data['Sentiment']==2)]
-
-# upsample minority classes
-df_negative_upsampled = resample(df_negative, replace=True, n_samples= 205, random_state=42)
-
-df_neutral_upsampled = resample(df_neutral, replace=True, n_samples= 205, random_state=42)
+    sentences = nltk.tokenize.sent_tokenize(text)
+    text = " ".join([word for word in word_tokenize(sentences[0]) if word not in stop_words])
+    text = " ".join([lzr.lemmatize(word) for word in word_tokenize(text)])
+    return text
 
 
-# Concatenate the upsampled dataframes with the neutral dataframe
-final_data = pd.concat([df_negative_upsampled,df_neutral_upsampled,df_positive])
+def model(processed_data):
+    df_neutral = processed_data[(processed_data["Sentiment"] == 1)]
+    df_negative = processed_data[(processed_data["Sentiment"] == 0)]
+    df_positive = processed_data[(processed_data["Sentiment"] == 2)]
 
-corpus = []
-for sentence in final_data['Sentence']:
-    corpus.append(sentence)
-corpus[0:5]
+    df_negative_upsampled = resample(df_negative, replace=True, n_samples=205, random_state=42)
+    df_neutral_upsampled = resample(df_neutral, replace=True, n_samples=205, random_state=42)
+    final_data = pd.concat([df_negative_upsampled, df_neutral_upsampled, df_positive])
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
-classifier = GaussianNB()
-classifier.fit(X_train, y_train)
+    corpus = []
+    for sentence in final_data["comment"]:
+        corpus.append(sentence)
+    corpus[0:5]
 
-y_pred = classifier.predict(X_test)
-cm = confusion_matrix(y_test, y_pred)
+    cv = CountVectorizer(max_features=1500)
+    X = cv.fit_transform(corpus).toarray()
+    y = final_data.iloc[:, -1].values
 
-nb_score = accuracy_score(y_test, y_pred)
-print('accuracy',nb_score)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+    classifier = GaussianNB()
+    classifier.fit(X_train, y_train)
+
+    y_pred = classifier.predict(X_test)
+    cm = confusion_matrix(y_test, y_pred)
+    nb_score = accuracy_score(y_test, y_pred)
+    print("accuracy", nb_score)
+
+
+def sentiment(data):
+    nltk.download("vader_lexicon")
+    sentiments = SentimentIntensityAnalyzer()
+    data["Positive"] = [sentiments.polarity_scores(i)["pos"] for i in data["comment"]]
+    data["Negative"] = [sentiments.polarity_scores(i)["neg"] for i in data["comment"]]
+    data["Neutral"] = [sentiments.polarity_scores(i)["neu"] for i in data["comment"]]
+    data["Compound"] = [sentiments.polarity_scores(i)["compound"] for i in data["comment"]]
+    score = data["Compound"].values
+    sentiment = []
+    for i in score:
+        if i >= 0.05:
+            sentiment.append("Positive")
+        elif i <= -0.05:
+            sentiment.append("Negative")
+        else:
+            sentiment.append("Neutral")
+    data["Sentiment"] = sentiment
+    return data
+
+
+def main():
+    data = pd.read_csv("comments.csv")
+    data1 = data.drop(["author_display_name", "comment_like_count"], axis=1)
+    data1 = sentiment(data1)
+    data2 = data1.drop(["Positive", "Negative", "Neutral", "Compound"], axis=1)
+
+    nltk.download("omw-1.4")
+    data_copy = data2.copy()
+    data_copy.comment = data_copy.comment.apply(lambda text: text_processing(text))
+
+    le = LabelEncoder()
+    data_copy["Sentiment"] = le.fit_transform(data_copy["Sentiment"])
+    model(data_copy)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
