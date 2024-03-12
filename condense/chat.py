a96cc54ed9067a9d4c6036c0b436ecefe267cf29
vikalp <vikalpdhalwal@gmail.com>
2024-03-12 18:22:21 +0530

    knowledge_base_updation

--- condense/chat.py
+++ condense/chat.py
@@ -39,18 +39,10 @@ def chat_bot():
 
         if best_match:
             answer: str = get_answer_for_question(best_match, knowledge_base)
-            print(f"Bot:{answer}")
+            print(f"Bot: {answer}")
 
         else:
-            print("Bot: I don't know the answer. Can you teach me?")
-            new_answer: str = input('Type the answer or "skip" to skip: ')
-
-            if new_answer.lower() != "skip":
-                knowledge_base["questions"].append(
-                    {"question": user_input, "answer": new_answer}
-                )
-                save_knowledge_base("knowledge_base.json", knowledge_base)
-                print("Thank you\n")
+            print("Bot: I don't know the answer. Please ask a relevent question?")
 
 
 if __name__ == "__main__":
