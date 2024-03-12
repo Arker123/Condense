86714008786f1748afaea88a056a951cf0e51c6a
Nandini-96 <113292455+Nandini-96@users.noreply.github.com>
2024-03-12 15:38:27 +0530

    Added My Notes Page

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,18 @@ import Landing from './pages/LandingPage';
 import LandingPage2 from './pages/LandingPage2';
 import ContactUs from './pages/ContactUs';
 import SignUp from './pages/SignUp';
+import SummaryPage from './pages/SummaryPage';
+import Dashboard from './pages/Dashboard';
+import MyNotes from './pages/MyNotes';
 
 /**
  * Renders the main application component.
  * @return {JSX.Element} The rendered App component.
  */
 function App() {
+// call refresh endpoint
+    // const { loading } = useLoadingWithRefresh();
+
     return (
         <div className="">
             <Routes>
@@ -17,6 +23,9 @@ function App() {
                 <Route exact path="/landing" element={<LandingPage2 />} />
                 <Route exact path="/contact" element={<ContactUs />} />
                 <Route exact path="/signup" element={<SignUp />} />
+                <Route exact path="/mynotes" element={<MyNotes/>} />
+                <Route exact path='/summary' element={<SummaryPage/>} />
+                <Route exact path='/dashboard' element={<Dashboard/>} />
             </Routes>
         </div>
     );
