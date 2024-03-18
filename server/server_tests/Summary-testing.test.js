a74134ff099427cc86f6951aa1958b37c8c539c1
n1ti <niti200005@gmail.com>
2024-03-18 20:19:48 +0530

    Backend database tests written

--- server/server_tests/Summary-testing.test.js
+++ server/server_tests/Summary-testing.test.js
@@ -6,57 +6,145 @@ const port = process.env.PORT || 5000;
 require("dotenv").config();
 
 beforeEach(async () => {
-    await mongoose.connect(process.env.MONGO_URL);
+  await mongoose.connect(process.env.MONGO_URL);
 });
 
 afterEach(async () => {
-    await mongoose.connection.close();
+  await mongoose.connection.close();
+  await new Promise(resolve => setTimeout(() => resolve(), 500)); // avoid jest open handle error
 });
 
 const server = app.listen(() => {
-    console.log(`Server is running on ${port} `);
+  console.log(`Server is running on ${port} `);
 });
 
 describe("Tests For fetch_All_summaries", () => {
-    test("should respond with 400 status code if User_ID is missing", async () => {
-        const response = await request(server)
-            .get("/summaries/getAll")
-            .send({});
-        expect(response.statusCode).toBe(400);
+  test("should respond with 400 status code if User_ID is missing", async () => {
+    const response = await request(server).get("/summaries/getAll").send({});
+    expect(response.statusCode).toBe(400);
+  });
+  test("should respond with 200 status code and give all summaries when correct userId given", async () => {
+    const response2 = await request(server).get("/summaries/getAll").send({
+      userId: "65ef4fd63ae8ba6151a918d8",
     });
+    const dummySummary = {
+      summary: { body: "This is a dummy summary!" },
+      videoId: "12ka4",
+    };
+    expect(response2.statusCode).toBe(200);
+    expect(response2._body).toEqual(
+      expect.arrayContaining([expect.objectContaining(dummySummary)])
+    );
+  });
 });
 
 describe("Tests For fetch_One_summary", () => {
-    test("should respond with 400 status code if User_ID is missing", async () => {
-        const response = await request(server)
-            .get("/summaries/getOne")
-            .send({});
-        expect(response.statusCode).toBe(400);
+  test("should respond with 400 status code if User_ID is missing", async () => {
+    const response = await request(server).get("/summaries/getOne").send({});
+    expect(response.statusCode).toBe(400);
+  });
+
+  test("should respond with 400 status code if summaryId is missing", async () => {
+    const response = await request(server)
+      .get("/summaries/getOne")
+      .send({ userId: "65ef4fd63ae8ba6151a918d8" });
+    expect(response.statusCode).toBe(400);
+  });
+
+  test("should respond with 200 status code and give correct summary when userId given", async () => {
+    const response = await request(server).get("/summaries/getOne").send({
+      userId: "65ef4fd63ae8ba6151a918d8",
+      summaryId: "65f8411537238b8f0d969c4e",
     });
+    const dummySummary = {
+      summary: { body: "This is a dummy summary!" },
+      videoId: "12ka4",
+    };
+    expect(response.statusCode).toBe(200);
+    expect(response._body).toEqual(
+      expect.arrayContaining([expect.objectContaining(dummySummary)])
+    );
+  });
 });
 
 describe("Tests For fetch_fav_summaries", () => {
-    test("should respond with 400 status code if User_ID is missing", async () => {
-        const response = await request(server)
-            .get("/summaries/getFav")
-            .send({});
-        expect(response.statusCode).toBe(400);
+  test("should respond with 400 status code if User_ID is missing", async () => {
+    const response = await request(server).get("/summaries/getFav").send({});
+    expect(response.statusCode).toBe(400);
+  });
+  test("should respond with 200 status code and give all summaries when correct userId given", async () => {
+    const response = await request(server).get("/summaries/getFav").send({
+      userId: "65ef4fd63ae8ba6151a918d8",
     });
+    const dummySummary = {
+      summary: { body: "This is a dummy summary!" },
+      videoId: "12ka4",
+    };
+    expect(response.statusCode).toBe(200);
+    expect(response._body).toEqual(
+      expect.arrayContaining([expect.objectContaining(dummySummary)])
+    );
+  });
 });
 
 describe("Tests For modify_fav_summaries", () => {
-    test("should respond with 400 status code if User_ID is missing", async () => {
-        const response = await request(server)
-            .post("/summaries/modifyFav")
-            .send({});
-        expect(response.statusCode).toBe(400);
+  test("should respond with 400 status code if User_ID is missing", async () => {
+    const response = await request(server)
+      .post("/summaries/modifyFav")
+      .send({});
+    expect(response.statusCode).toBe(400);
+  });
+  test("should respond with 400 status code if summaryId is missing", async () => {
+    const response = await request(server)
+      .post("/summaries/modifyFav")
+      .send({ userId: "65ef4fd63ae8ba6151a918d8" });
+    expect(response.statusCode).toBe(400);
+  });
+
+  test("should favourite the summary and be included in the get(/summaries/getFav) request", async () => {
+    request(server).post("/summaries/modifyFav").send({
+      userId: "65ef4fd63ae8ba6151a918d8",
+      summaryId: "65f8411537238b8f0d969c4e",
     });
+    const response2 = await request(server).get("/summaries/getFav").send({
+      userId: "65ef4fd63ae8ba6151a918d8",
+    });
+    const dummySummary = {
+      summary: { body: "This is a dummy summary!" },
+      videoId: "12ka4",
+    };
+    expect(response2.statusCode).toBe(200);
+    expect(response2._body).toEqual(
+      expect.arrayContaining([expect.objectContaining(dummySummary)])
+    );
+  });
 });
 
 describe("Tests For save_summary", () => {
-    test("should respond with 400 status code if User_ID is missing", async () => {
-        const response = await request(server).post("/summaries/save").send({});
-        expect(response.statusCode).toBe(400);
+  test("should respond with 400 status code if User_ID is missing", async () => {
+    const response = await request(server).post("/summaries/save").send({});
+    expect(response.statusCode).toBe(400);
+  });
+
+  test("should save summary to database and be included in the get(/summaries/getAll) request", async () => {
+    request(server).post("/summaries/save").send({
+      userId: "65ef4fd63ae8ba6151a918d8",
+      videoId: "12ka4",
+      summaryBody: "This is a dummy summary! Successful.",
     });
+    const response2 = await request(server).get("/summaries/getAll").send({
+      userId: "65ef4fd63ae8ba6151a918d8",
+    });
+    const dummySummary = {
+      summary: { body: "This is a dummy summary! Successful." },
+      videoId: "12ka4",
+    };
+    expect(response2.statusCode).toBe(200);
+    expect(response2._body).toEqual(
+      expect.arrayContaining([expect.objectContaining(dummySummary)])
+    );
+  });
 });
+
 server.close();
+
