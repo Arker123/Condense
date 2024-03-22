4e358530ef5d84585a478299f22a20c69e571f98
Virat <2021csb1220@iitrpr.ac.in>
2024-03-22 18:01:37 +0530

    Audio to Summary Page

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -15,6 +15,10 @@ const Dashboard = () => {
 
   const navigate = useNavigate();
   const [url,setUrl] = useState("")
+  const [audio,setAudio] = useState(null)
+  const [video , setVideo] = useState(null)
+
+  const [selectedButton, setSelectedButton] = useState("YouTube Summary");
   const menus = [
     { name: "Dashboard", link: "/", icon: IoMdHome },
     { name: "Chat with AI", link: "/", icon: BsChatFill },
@@ -23,6 +27,98 @@ const Dashboard = () => {
     { name: "Chrome Extension", link: "/", icon: IoExtensionPuzzle },
   ];
   const [open, setOpen] = useState(true);
+  const handleButtonClick = (buttonName) => {
+    setSelectedButton(buttonName);
+  };
+
+  const renderContent = () => {
+    switch (selectedButton) {
+      case "YouTube Summary":
+        return (
+          <div className="bg-white h-[310px] w-[800px] ml-[450px] flex flex-col mt-[50px] rounded-2xl">
+            <motion.p
+              initial={{ opacity: 0, x: -100 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1 }}
+              className="text-extrabold text-[55px] font-serif flex items-center justify-center mt-7"
+            >
+              Absorb Videos Better Now
+            </motion.p>
+            <div className="flex flex-row gap-4">
+              <input
+                type="link"
+                onChange={(e)=>setUrl(e.target.value)}
+                value={url}
+                placeholder="Paste Youtube Video link Here!"
+                className="ml-[50px] pl-2 mt-[60px] text-black bg-gray-200 text-small h-[65px] w-[490px] rounded-xl overflow-hidden"
+              />
+              <button
+                onClick={()=>{navigate('/summary',{state:url})}}
+                className="cursor-pointer text-black bg-gradient-to-b from-red-400 to-red-900 rounded-xl shadow-lg w-[150px] h-[50px] mt-[68px]"
+              >
+                Start Summarizing
+              </button>
+            </div>
+          </div>
+        );
+        case "Audio to Summary":
+          return (
+            <div className="bg-white h-[310px] w-[800px] ml-[450px] flex flex-col mt-[50px] rounded-2xl">
+              <motion.p
+                initial={{ opacity: 0, x: -100 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 1 }}
+                className="text-extrabold text-[55px] font-serif flex items-center justify-center mt-7"
+              >
+                Absorb Audio Better Now
+              </motion.p>
+              <div className="flex flex-row gap-4">
+                <input
+                  type="file"
+                  accept="audio/*"
+                  onChange={(e) => setAudio(e.target.files[0])}
+                  className="ml-[50px] pl-2 mt-[60px] text-black bg-gray-200 text-small h-[50px] w-[490px] rounded-xl overflow-hidden"
+                />
+                <button
+                  onClick={()=>{navigate('/summary',{state:audio})}}
+                  className="cursor-pointer text-black bg-gradient-to-b from-red-400 to-red-900 rounded-xl shadow-lg w-[150px] h-[50px] mt-[60px]"
+                >
+                  Start Summarizing
+                </button>
+              </div>
+            </div>
+          );
+        case "Video to Summary":
+          return (
+            <div className="bg-white h-[310px] w-[800px] ml-[450px] flex flex-col mt-[50px] rounded-2xl">
+              <motion.p
+                initial={{ opacity: 0, x: -100 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 1 }}
+                className="text-extrabold text-[55px] font-serif flex items-center justify-center mt-7"
+              >
+                Absorb Videos Better Now
+              </motion.p>
+              <div className="flex flex-row gap-4">
+                <input
+                  type="file"
+                  accept="video/mp4"
+                  onChange={(e) => setVideo(e.target.files[0])}
+                  className="ml-[50px] pl-2 mt-[60px] text-black bg-gray-200 text-small h-[50px] w-[490px] rounded-xl overflow-hidden"
+                />
+                <button
+                  onClick={()=>{navigate('/summary',{state:video})}}
+                  className="cursor-pointer text-black bg-gradient-to-b from-red-400 to-red-900 rounded-xl shadow-lg w-[150px] h-[50px] mt-[60px]"
+                >
+                  Start Summarizing
+                </button>
+              </div>
+            </div>
+          );
+      default:
+        return null;
+    }
+  };
 
   return (
     <section className="flex gap-6  bg-gradient-to-b from-red-400 via-red-900 to-gray-950 h-screen">
@@ -78,7 +174,7 @@ const Dashboard = () => {
         </div>
       </div>
 
-      <div>
+      <div className="">
         <div className=" flex flex-row items-center justify-center gap-4 mt-5  ml-[1150px]">
           <div className="w-30 h-10  rounded-3xl bg-white flex items-center px-4">
             <div className=" flex flex-row gap-2">
@@ -98,31 +194,34 @@ const Dashboard = () => {
             <CgProfile />
           </div>
         </div>
-
-        <div className="bg-white h-[310px] w-[800px] ml-[400px] flex flex-col mt-[150px] rounded-2xl">
-          <motion.p
-            initial={{ opacity: 0, x: -100 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 1 }}
-            className="text-extrabold text-[55px] font-serif flex items-center justify-center mt-7"
-          >
-            Absorb Videos Better Now
-          </motion.p>
-          <div className="flex flex-row gap-4">
-            <input
-              type="link"
-              onChange={(e)=>setUrl(e.target.value)}
-              value={url}
-              placeholder="Paste Youtube Video link Here!"
-              className="ml-[50px] pl-2 mt-[60px] text-black bg-gray-200 text-small h-[65px] w-[490px] rounded-xl overflow-hidden"
-            />
+        <div className="flex flex-col items-center justify-center">
+        <div className="bg-white h-[50px] w-[600px] ml-[450px] mt-[100px] flex flex-row justify-evenly rounded-2xl">
             <button
-              onClick={()=>{navigate('/summary',{state:url})}}
-              className="cursor-pointer text-black bg-gradient-to-b from-red-400 to-red-900 rounded-xl shadow-lg w-[150px] h-[50px] mt-[68px]"
+              className={`text-sm text-black font-medium px-4 py-2 rounded-md border border-gray-300 ${
+                selectedButton === "YouTube Summary" ? "bg-red-500" : ""
+              }`}
+              onClick={() => handleButtonClick("YouTube Summary")}
             >
-              Start Summarizing
+              YouTube Summary
+            </button>
+            <button
+              className={`text-sm text-black font-medium px-4 py-2 rounded-md border border-gray-300 ${
+                selectedButton === "Audio to Summary" ? "bg-red-500" : ""
+              }`}
+              onClick={() => handleButtonClick("Audio to Summary")}
+            >
+              Audio to Summary
+            </button>
+            <button
+              className={`text-sm text-black font-medium px-4 py-2 rounded-md border border-gray-300 ${
+                selectedButton === "Video to Summary" ? "bg-red-500" : ""
+              }`}
+              onClick={() => handleButtonClick("Video to Summary")}
+            >
+              Video to Summary
             </button>
           </div>
+          {renderContent()}
         </div>
       </div>
     </section>
