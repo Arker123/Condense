22032b049193ef35bdccbaf008a211849674b34c
ARinger22 <gautamarpit1204@gmail.com>
2024-03-26 18:28:10 +0530

    updated required changes

--- condense/train.py
+++ condense/train.py
@@ -1,12 +1,9 @@
 import os
-import re
 import sys
 import logging
 from typing import Tuple
 from datetime import datetime
 
-import nltk
-import emoji
 import numpy as np
 import torch
 import pandas as pd
@@ -16,36 +13,11 @@ from sklearn.model_selection import train_test_split
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
+from condense.data_preprocessing import preprocess_data
 from condense.sentimentLSTM_model import SentimentLSTM
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
-
-
-def preprocess_data(comment: str) -> str:
-    """
-    This function preprocesses the input comment by:
-    - Converting emojis to text representations
-    - Removing special characters except for selected punctuation
-    - Normalizing whitespace
-    - Converting text to lowercase
-    - Replacing specific emoji-related words with standardized tokens ('like' for 'heart', 'smile' for 'smile')
-
-    Parameters:
-        comment (str): The input comment to preprocess.
-
-    Returns:
-        str: The preprocessed comment.
-    """
-    comment = emoji.demojize(comment)
-    comment = re.sub("[^a-zA-Z0-9.,;:()'*`\"\\s]", "", comment)
-    comment = re.sub("\\s+", " ", comment)
-    comment = comment.strip().lower()
-    words = nltk.tokenize.word_tokenize(comment)
-    words = ["like" if "heart" in word else word for word in words]
-    words = ["smile" if "smile" in word else word for word in words]
-    comment = " ".join(words)
-    return comment
 
 
 def train_model(
@@ -180,7 +152,6 @@ def tokenized_text(data: pd.DataFrame) -> Tuple[np.ndarray, int, Tokenizer]:
 
 def main():
     logging.basicConfig(level=logging.DEBUG)
-    nltk.download("punkt")
     # https://www.kaggle.com/datasets/abhi8923shriv/sentiment-analysis-dataset?select=train.csv
     data = pd.read_csv("train.csv")
     data.dropna(inplace=True)
