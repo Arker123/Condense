78cf6192a93190e24d2cbf510de81e6454497671
Arpit Kumar Gautam <gautamarpit1204@gmail.com>
2024-03-26 17:21:41 +0530

    Update condense/model.py
    Co-authored-by: Arnav Kharbanda <94680887+Arker123@users.noreply.github.com>

--- condense/model.py
+++ condense/model.py
@@ -12,7 +12,6 @@ import torch
 import pandas as pd
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
