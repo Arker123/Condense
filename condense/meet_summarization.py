13f7a28a3f9bc13a2ae04aff9630c3bde732f7d5
ARinger22 <gautamarpit1204@gmail.com>
2024-04-06 14:31:59 +0530

    updated transcriptor code

--- condense/meet_summarization.py
+++ condense/meet_summarization.py
@@ -1,30 +1,41 @@
+import time
+from threading import Thread
+
 import soundcard as sc
 import soundfile as sf
 from transformers import pipeline
-from threading import Thread
 
-filename = "output.wav"
+model = pipeline("automatic-speech-recognition", model="openai/whisper-medium")
 samplerate = 48000
-record_sec = 10
+record_sec = 15
 
-def record_and_transcribe():
+
+def transcription(filename):
+    print("Transcribing...")
+    transcribed_text = model(filename)
+    print("Transcript:", transcribed_text["text"])
+
+
+def record():
     print("Recording...")
     with sc.get_microphone(id=str(sc.default_speaker().name), include_loopback=True).recorder(
-            samplerate=samplerate) as mic:
+        samplerate=samplerate
+    ) as mic:
+        filename = f"output_{time.time()}.wav"
         data = mic.record(numframes=samplerate * record_sec)
         sf.write(file=filename, data=data[:, 0], samplerate=samplerate)
-
-    print("Transcribing...")
-    whisper_asr = pipeline("automatic-speech-recognition")
-    transcribed_text = whisper_asr(filename)
-    print("Transcript:", transcribed_text["text"])
+        transcription(filename)
 
 
 def main():
     while True:
-        thread = Thread(target=record_and_transcribe)
-        thread.start()
-        # thread.join()
+        try:
+            thread = Thread(target=record)
+            thread.start()
+            time.sleep(record_sec - 1)
+        except KeyboardInterrupt:
+            break
+
 
 if __name__ == "__main__":
     main()
