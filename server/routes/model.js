38f613b1552ae9de1c10235256e5ccdedbbf0ac1
Dhananjay <dhananjaygoel23@gmail.com>
2024-04-07 17:44:23 +0530

    updated

--- server/routes/model.js
+++ server/routes/model.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const app = express();
 
 app.use(express.json());
@@ -7,23 +7,26 @@ app.use(express.json());
 const getApiKey = () => {
     const apiKey = process.env.API_KEY;
     if (!apiKey) {
-        throw new Error('API key not found in environment variables');
+        throw new Error("API key not found in environment variables");
     }
     return apiKey;
 };
 
-app.post('/predict_sentiment', async (req, res) => {
+app.post("/predict_sentiment", async (req, res) => {
     const comment = req.body.comment;
     try {
-        const api_key = getApiKey();
-        const response = await axios.post('https://condense-4eevndrdnq-em.a.run.app/analyze_sentiment', {api_key, comment });        
+        const apiKey = getApiKey();
+        const response = await axios.post(
+            "https://condense-4eevndrdnq-em.a.run.app/analyze_sentiment",
+            { apiKey, comment },
+        );
         const prediction = response.data.prediction;
         res.json({ prediction });
     } catch (error) {
-        res.status(500).json({ error: 'Internal server error' });
+        res.status(500).json({ error: "Internal server error" });
     }
 });
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+    console.log("Server is running on port 3000");
 });
