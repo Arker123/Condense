24db106f4e92d20a5db631773174424dbc8e93b7
Kritika Bansal <101122444+kritika220703@users.noreply.github.com>
2024-04-07 16:54:38 +0530

    Update client/src/pages/MyNotesAndSummaries.jsx
    Co-authored-by: Arnav Kharbanda <94680887+Arker123@users.noreply.github.com>

--- client/src/pages/MyNotesAndSummaries.jsx
+++ client/src/pages/MyNotesAndSummaries.jsx
@@ -72,8 +72,8 @@ const MyNotesAndSummaries = () => {
                 <NoteAndSummary 
                     title="Your Note Title 1"  
                     youtubeUrl="https://www.youtube.com/watch?v=yj3lm00cZSg&ab_channel=SonyLIV" 
-                    note="kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhkkkkkkkk"
-                    summary="hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh"
+                    note="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+                    summary="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
                 />
                 <NoteAndSummary title="Your Note Title 2"  youtubeUrl="https://www.youtube.com/watch?v=yj3lm00cZSg&ab_channel=SonyLIV"/>
                 <NoteAndSummary title="Your Note Title 2"  youtubeUrl="https://www.youtube.com/watch?v=yj3lm00cZSg&ab_channel=SonyLIV"/>
