52990ba90a7b0afbaf2993b727f432bc7204620c
ARinger22 <gautamarpit1204@gmail.com>
2024-04-09 13:07:58 +0530

    model inside function

--- condense/meet_transciptor.py
+++ condense/meet_transciptor.py
@@ -1,37 +1,50 @@
+import os
 import time
-import shutil
+import warnings
 from threading import Thread
 
-import whisper
 import soundcard as sc
 import soundfile as sf
+import whisper
 
 from condense.youtube_audio_extractor import get_transcript
 
-model = whisper.load_model("base")
+warnings.filterwarnings("ignore", category=sc.SoundcardRuntimeWarning)
+
 samplerate = 48000
 record_sec = 15
 
 
-def record():
+def load_model():
+    model = whisper.load_model("base")
+    return model
+
+
+def record(model):
     print("Recording...")
-    with sc.get_microphone(id=str(sc.default_speaker().name), include_loopback=True).recorder(
-        samplerate=samplerate
-    ) as mic:
-        filename = f"output_{time.time()}.wav"
-        data = mic.record(numframes=samplerate * record_sec)
-        sf.write(file=filename, data=data[:, 0], samplerate=samplerate)
-        transcribed_text = get_transcript(model, ".", filename)
-        print(transcribed_text)
-        shutil.rmtree(filename)
+    filename = f"output_{time.time()}.wav"
+    try:
+        with sc.get_microphone(id=str(sc.default_speaker().name), include_loopback=True).recorder(
+            samplerate=samplerate
+        ) as mic:
+            data = mic.record(numframes=samplerate * record_sec)
+            sf.write(file=filename, data=data[:, 0], samplerate=samplerate)
+            transcribed_text = get_transcript(model, ".", filename)
+            print(transcribed_text)
+    except Exception as e:
+        print(f"Error occurred: {e}")
+    finally:
+        if os.path.exists(filename):
+            os.remove(filename)
 
 
 def main():
+    model = load_model()
     while True:
         try:
-            thread = Thread(target=record)
+            thread = Thread(target=record, args=(model,))
             thread.start()
-            time.sleep(record_sec - 1)
+            time.sleep(record_sec)
         except KeyboardInterrupt:
             break
 
