4fd3ef749e83728a05ad9de04f7aac1e6c3504b9
ARinger22 <gautamarpit1204@gmail.com>
2024-04-16 18:09:46 +0530

    tests passed

--- condense/external_voice_transcriptor.py
+++ condense/external_voice_transcriptor.py
@@ -13,8 +13,6 @@ from condense.youtube_audio_extractor import load_model, get_transcript
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
-recordings = Queue()
-
 CHANNELS = 1
 FRAME_RATE = 16000
 RECORD_SECONDS = 10
@@ -23,21 +21,20 @@ SAMPLE_SIZE = 2
 CHUNK = 1024
 
 
-def speech_recognition(whisper_asr: whisper.model):
+def speech_recognition(whisper_asr: whisper.model, frames):
+    logger.info(f"Transcribing.....")
     filename = f"output_{time.time()}.wav"
-    while not recordings.empty():
-        frames = recordings.get()
-        all_frames = b"".join(frames)
-        with wave.open(filename, "wb") as wf:
-            wf.setnchannels(CHANNELS)
-            wf.setsampwidth(SAMPLE_SIZE)
-            wf.setframerate(FRAME_RATE)
-            wf.writeframes(all_frames)
-        transcribed_text = get_transcript(whisper_asr, ".", filename)
-        print(transcribed_text)
+    all_frames = b"".join(frames)
+    with wave.open(filename, "wb") as wf:
+        wf.setnchannels(CHANNELS)
+        wf.setsampwidth(SAMPLE_SIZE)
+        wf.setframerate(FRAME_RATE)
+        wf.writeframes(all_frames)
+    transcribed_text = get_transcript(whisper_asr, ".", filename)
+    print(transcribed_text)
 
 
-def record_microphone():
+def record_microphone(whisper_asr: whisper.model):
     logger.info(f"Recording.....")
     p = pyaudio.PyAudio()
     stream = p.open(format=AUDIO_FORMAT, channels=CHANNELS, rate=FRAME_RATE, input=True, frames_per_buffer=CHUNK)
@@ -45,7 +42,7 @@ def record_microphone():
     data = stream.read(CHUNK)
     frames.append(data)
     if len(frames) >= (FRAME_RATE * RECORD_SECONDS) / CHUNK:
-        recordings.put(frames.copy())
+        speech_recognition(whisper_asr, frames)
         frames = []
 
     stream.stop_stream()
@@ -57,12 +54,8 @@ def start_recording():
     whisper_asr = load_model()
     while True:
         try:
-            record = Thread(target=record_microphone)
+            record = Thread(target=record_microphone, args=(whisper_asr,))
             record.start()
-
-            transcribe = Thread(target=speech_recognition, args=(whisper_asr,))
-            transcribe.start()
-
             time.sleep(RECORD_SECONDS - 1)
         except KeyboardInterrupt:
             break
