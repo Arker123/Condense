67bdabdacc845053bedb916ea811f96873a33c4c
ARinger22 <gautamarpit1204@gmail.com>
2024-04-17 02:52:32 +0530

    final code

--- condense/external_voice_transcriptor.py
+++ condense/external_voice_transcriptor.py
@@ -1,8 +1,8 @@
+import os
 import sys
 import time
 import wave
 import logging
-from queue import Queue
 from threading import Thread
 
 import pyaudio
@@ -13,15 +13,23 @@ from condense.youtube_audio_extractor import load_model, get_transcript
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
+# Constants for audio recording
 CHANNELS = 1
 FRAME_RATE = 16000
-RECORD_SECONDS = 10
+RECORD_SECONDS = 15
 AUDIO_FORMAT = pyaudio.paInt16
 SAMPLE_SIZE = 2
 CHUNK = 1024
 
 
-def speech_recognition(whisper_asr: whisper.model, frames):
+def speech_recognition(whisper_asr: whisper.model, frames: list):
+    """
+    Transcribe audio frames using the provided whisper model.
+
+    Args:
+        whisper_asr (whisper.model): The pre-trained model for speech recognition.
+        frames (list): List of audio frames to be transcribed.
+    """
     logger.info(f"Transcribing.....")
     filename = f"output_{time.time()}.wav"
     all_frames = b"".join(frames)
@@ -32,18 +40,27 @@ def speech_recognition(whisper_asr: whisper.model, frames):
         wf.writeframes(all_frames)
     transcribed_text = get_transcript(whisper_asr, ".", filename)
     print(transcribed_text)
+    if os.path.exists(filename):
+        os.remove(filename)
 
 
 def record_microphone(whisper_asr: whisper.model):
+    """
+    Record audio from the microphone and transcribe it using the provided model.
+
+    Args:
+        whisper_asr (whisper.model): The pre-trained model for speech recognition.
+    """
     logger.info(f"Recording.....")
     p = pyaudio.PyAudio()
     stream = p.open(format=AUDIO_FORMAT, channels=CHANNELS, rate=FRAME_RATE, input=True, frames_per_buffer=CHUNK)
     frames = []
-    data = stream.read(CHUNK)
-    frames.append(data)
-    if len(frames) >= (FRAME_RATE * RECORD_SECONDS) / CHUNK:
-        speech_recognition(whisper_asr, frames)
-        frames = []
+    while True:
+        data = stream.read(CHUNK)
+        frames.append(data)
+        if len(frames) >= (FRAME_RATE * RECORD_SECONDS) / CHUNK:
+            speech_recognition(whisper_asr, frames)
+            break
 
     stream.stop_stream()
     stream.close()
@@ -51,12 +68,16 @@ def record_microphone(whisper_asr: whisper.model):
 
 
 def start_recording():
+    """Start the audio recording process."""
     whisper_asr = load_model()
     while True:
         try:
+            # Create a new thread for recording audio
             record = Thread(target=record_microphone, args=(whisper_asr,))
             record.start()
-            time.sleep(RECORD_SECONDS - 1)
+
+            # Wait for the specified recording duration before starting the next recording
+            time.sleep(RECORD_SECONDS)
         except KeyboardInterrupt:
             break
 
